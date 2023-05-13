00d1ea1de4707111641a69e943f7fa59f2352a13
ilan990 <77548546+ilan990@users.noreply.github.com>
2023-05-13 21:33:44 +0200

    Ajout de la fixture auteur

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,16 +5,28 @@ namespace App\DataFixtures;
 use App\Entity\Book;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Author;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        // Création des Auteurs
+        $listAuthor = [];
+        for ($i = 0; $i < 10; $i++){
+            $author = new Author();
+            $author -> setFirstName("Prénom ". $i);
+            $author-> setLastName("Nom ". $i);
+            $manager->persist($author);
+            $listAuthor[] = $author;
+        }
 
+        // Création des livres
         for ($i = 0; $i < 20; $i++){
             $book = new Book();
             $book ->setTitle("Titre " . $i);
             $book -> setCoverText("Quatrième de couverture numéro : " .$i);
+            $book ->setAuthor($listAuthor[array_rand($listAuthor)]);
             $manager->persist($book);
         }
         $manager->flush();
