6a43c72e04dec80353a52a956ef49cad9773918f
ilan990 <77548546+ilan990@users.noreply.github.com>
2023-05-13 20:25:04 +0200

    Création de la première API

---
+++ src/Controller/BookController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BookRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class BookController extends AbstractController
+{
+    #[Route('/api/books', name: 'app_book',methods: ['GET'])]
+    public function getAllBooks(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $booklist = $bookRepository ->findAll();
+
+        $jsonBookList = $serializer->serialize($booklist, 'json');
+        return new JsonResponse([ $jsonBookList, Response::HTTP_OK, [], true ]);
+    }
+}
